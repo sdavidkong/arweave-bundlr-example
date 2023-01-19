dc51f26c95c778d659d59fb4cb894fb1e1014b00
S David Kong <SDAVIDKONG@gmail.com>
2023-01-18 21:30:02 -0800

    added connect button for bundlr

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { providers, utils } from "ethers";
+import { WebBundlr } from "@bundlr-network/client";
+import { useState, useRef } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  async function initialize() {
+    await window.ethereum.enable();
+    const provider = new providers.Web3Provider(window.ethereum);
+    await provider._ready();
+
+    const bundlr = new WebBundlr(
+      "https://node1.bundlr.network",
+      "matic",
+      provider
+    );
+    await bundlr.ready();
+  }
+  return (
+    <div>
+      <button onClick={initialize}>initialize</button>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
